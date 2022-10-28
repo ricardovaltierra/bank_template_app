287521481aec06a1d3ee9bb3565fa1175a26052b
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-10-28 07:29:53 -0500

    Add custom styles with js file and set Navbar and Hero structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import styles from './style';
 
-const App = () => {
-  return (
-    <div>
-      
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
     </div>
-  )
-};
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        'Hero Section'
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
