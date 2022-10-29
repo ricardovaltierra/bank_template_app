c598e72478fe788b3439406f1492f972643f9f08
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-10-29 16:26:15 -0500

    Set mobile toggle menu imgs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { close, logo, menu } from '../assets';
 import { navLinks } from  '../constants';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
@@ -15,6 +16,15 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="sm-hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu }
+          alt="menu"
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((prev) => !prev)}
+        />
+      </div>
     </nav>
   )
 }
