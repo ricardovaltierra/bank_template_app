506d892787050ccb0520708de94dce1830872cf1
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-02 14:19:41 -0600

    Add gradient to section as background and missing classes

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,9 +3,8 @@ import styles from "../style";
 import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => (
-    <section id="clients" className={`${styles.paddingY} ${styles.flexCenter}`}>
-      {/* TODO  gradient*/}
-      <div className="" />
+    <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
 
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
         <h1 className={styles.heading2}>
